90089d433aab077efa2993a06d26377366314827
ShunshunL <shunshunlyu@protonmail.com>
2020-04-22 19:09:35 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your code here
+
+function submitData(name, email) {
+  return fetch('http://localhost:3000/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify({name, email})
+  })
+            .then(response => {
+              return response.json()
+            })
+            .then(obj => {
+              document.body.innerHTML = obj["id"]
+            })
+            .catch(e => {
+              document.body.innerHTML = e.message
+            })
+}
